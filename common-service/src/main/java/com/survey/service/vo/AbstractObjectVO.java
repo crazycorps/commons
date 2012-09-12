e71c90918ffea64ad3106c82763af16fe134064d
corps <crazycorps@163.com>
2012-09-12 18:28:37 +0800

    sn method

--- common-service/src/main/java/com/survey/service/vo/AbstractObjectVO.java
+++ common-service/src/main/java/com/survey/service/vo/AbstractObjectVO.java
@@ -2,6 +2,11 @@ package com.survey.service.vo;
 
 import java.io.Serializable;
 
+import org.apache.commons.codec.binary.Hex;
+
+import com.survey.tools.encrypt.AESCoder;
+import com.survey.tools.util.BitUtils;
+
 @SuppressWarnings("serial")
 public abstract class AbstractObjectVO<T> implements Serializable {
 	
@@ -17,6 +22,26 @@ public abstract class AbstractObjectVO<T> implements Serializable {
 
 	public void setEntity(T entity) {
 		this.entity = entity;
+	}
+	
+	public static String toSn(long id,byte[] secretKey){
+		
+		try {
+			return Hex.encodeHexString(AESCoder.encrypt(BitUtils.long2bytes(id), secretKey));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static long parseSn(String sn,byte[] secretKey){
+		try {
+			byte[] data = AESCoder.decrypt(sn.getBytes(), secretKey);
+			return BitUtils.bytes2long(data, 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1L;
 	}
 
 }
